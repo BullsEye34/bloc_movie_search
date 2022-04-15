9e7ffb0f52fa0abc49f7655d742f7b4d03c3616c
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-15 18:44:26 +0530

    BLOC For ERROR and Trending

--- lib/bloc/movies_bloc.dart
+++ lib/bloc/movies_bloc.dart
@@ -1,14 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:tmdb/models/data.dart';
+import 'package:http/http.dart' as http;
 
 part 'movies_event.dart';
 part 'movies_state.dart';
 
 class MoviesBloc extends Bloc<MoviesEvent, MoviesState> {
+  var client = http.Client();
+
   MoviesBloc() : super(Initial()) {
-    on<MoviesEvent>((event, emit) {
+    on<GetTrending>((event, emit) {
       // TODO: implement event handler
+      emit(Loading());
+      try {
+        client
+            .get(Uri.parse(
+                "https://api.themoviedb.org/3/trending/movies/week?api_key=43236c9b4ffaa78012ee092b4e4f74d8&language=en-US&page=1&include_adult=false"))
+            .then((value) {
+          emit(Loaded(movies: [DataModelFromJson(value.body)]));
+        });
+      } catch (e) {
+        emit(Error(message: e.toString()));
+      }
     });
   }
 
--- lib/bloc/movies_state.dart
+++ lib/bloc/movies_state.dart
@@ -11,6 +11,15 @@ class Initial extends MoviesState {}
 
 class Loading extends MoviesState {}
 
+class Error extends MoviesState {
+  final String message;
+
+  Error({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
+
 class Loaded extends MoviesState {
   final List<DataModel> movies;
 
